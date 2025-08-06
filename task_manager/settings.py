d4093ce547845833561cf64ed2b3a4ecff3120c4
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-08-06 18:24:34 +0500

    feat: add users app

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_bootstrap5',
     'task_manager',
+    'task_manager.users',
 ]
 
 MIDDLEWARE = [
@@ -144,3 +145,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,8 +18,12 @@ from django.contrib import admin
 from django.urls import path
 
 from task_manager import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
