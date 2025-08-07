66a2cce0da2ab1c79e0ef7db1c3963ea17559ed0
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-08-07 10:58:33 +0500

    feat: implement URL routing

---
+++ task_manager/users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from task_manager.users import views
+
+
+urlpatterns = [
+    path('', views.UsersIndexView.as_view(), name='users_index'),
+    path('create/', views.UserCreateView.as_view(), name='user_create'),
+    path('<int:pk>/update/', views.UserUpdateView.as_view(), name='user_update'),
+    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='user_delete'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
