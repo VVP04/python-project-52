31013012cdaabd913517518f9b0e357219dd8954
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-08-10 12:20:36 +0500

    fix: correct login URL path in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -147,6 +147,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = '/login/'
+LOGIN_URL = '/users/login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
