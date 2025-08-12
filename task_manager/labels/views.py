10a5014ff454c379cf8db31c88393d34cd9914dc
Vladimir Plesovskih <vova.plesovkikh@gmail.com>
2025-08-12 16:52:11 +0500

    feat(labels): implement CRUD operations for Label

---
+++ task_manager/labels/views.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.core.exceptions import ValidationError
+from django.contrib import messages
+from django.shortcuts import redirect
+
+from .models import Label
+from .forms import LabelForm
+
+
+class LabelListView(LoginRequiredMixin, ListView):
+    model = Label
+    template_name = 'labels/index.html'
+    context_object_name = 'labels'
+
+
+class LabelCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Label
+    form_class = LabelForm
+    template_name = 'labels/create.html'
+    success_url = reverse_lazy('labels_index')
+    success_message = _("Label created successfully")
+
+
+class LabelUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Label
+    form_class = LabelForm
+    template_name = 'labels/update.html'
+    success_url = reverse_lazy('labels_index')
+    success_message = _("Label updated successfully")
+
+
+class LabelDeleteView(LoginRequiredMixin, DeleteView):
+    model = Label
+    template_name = 'labels/delete.html'
+    success_url = reverse_lazy('labels_index')
+    success_message = _("Label deleted successfully")
+    
+    def delete(self, *args, **kwargs):
+        if self.tasks.exists():
+            raise ValidationError(_("It is impossible to delete the label because it is being used"))
+        super().delete(*args, **kwargs)
